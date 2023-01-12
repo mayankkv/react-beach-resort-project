fc61cc3b0a18e67f23035be8db4e900a4d3e15fa
mayankkv <111362321+mayankkv@users.noreply.github.com>
2023-01-12 18:01:56 +0530

    Solved access token error

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,14 @@
 const contentful = require("contentful");
 
+if(process.env.REACT_APP_API_SPACE){
+  module.exports = contentful.createClient({
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  })
+}
+/*
 export default contentful.createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
+*/
